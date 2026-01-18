6bf71fe238c626ff09a278de30f709080df211eb
alexcleveland <alexcleveland5@gmail.com>
2026-01-18 10:44:34 -0700

    Turtle functionality

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,18 @@
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+from turtle import Turtle
+import random
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, score):
+        super().__init__()
+        self.color(random.choice(COLORS))
+        self.shape("square")
+        self.penup()
+        self.size = (1, 2)
+        rand_y = random.randint(-260, 280)
+        self.setheading(180)
+        self.goto(300, rand_y)
+
--- scoreboard.py
+++ scoreboard.py
@@ -4,10 +4,4 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.penup()
-        self.shape("turtle")
-        self.goto(0, -280)
-        self.setheading(90)
-
-    def move_up(self):
-        self.forward(10)
+        self.score = 0
